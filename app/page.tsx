4e256245cdf5f0f33aa20bba5deb082245a9a403
Leo Diaz <leoangelo.diaz@gmail.com>
2023-12-26 23:04:28 +0800

    add sample fetch call

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useEffect } from "react"
+import { fetchWithResponse } from "./_utils/rest-client";
 
 export default function Index() {
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api/docsmit/auth')
+      const response = await fetchWithResponse('/api/docsmit/auth')
+      console.log('xx response - ', response)
     }
 
     fetchData()
@@ -13,7 +15,7 @@ export default function Index() {
 
   return (
     <div className="container mx-auto max-w-full sm:px-4 md:px-10 mt-8">
-      <h1 className="text-3xl font-bold mb-4">Docsmit Demo</h1>
+      <h1 className="text-3xl font-bold mb-4">Docsmit Integration Demo</h1>
       
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-full text-sm">
         Get Test Token
